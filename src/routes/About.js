903d32d313c8269a2c425b4b33d1f733a0fdc4dc
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-04-23 11:51:21 +0400

    add pricing cards with hover effects

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const About = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg2 heading = "ABOUT." 
+                text = "I`m a Front-END Developer"/>
       <Footer/>
     </div>
   )
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
 import HeroImg2 from '../components/HeroImg2'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg2 heading = "PROJECTS."text="Some of my recent works"/>
+      <PricingCard/>
       <Footer/>
     </div>
   )
